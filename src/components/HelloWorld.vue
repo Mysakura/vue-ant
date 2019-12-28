0d5310d74a0e5a953e74e49f1c644fa2af70cbfc
Mysakura <3110320051@qq.com>
2019-12-28 19:35:17 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }}<router-link to="/login">去登陆</router-link></h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
